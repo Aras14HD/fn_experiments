a61b7c106948db4eded63208a3964745a2cd496e
Aras14HD <aras14k5@gmail.com>
2023-10-14 11:20:55 +0200

    Generalize Curry and useless FnOnce

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,58 @@
 #![feature(fn_traits, unboxed_closures, tuple_trait)]
 
-use std::marker::{Tuple, PhantomData};
-struct Curry<V, F: FnOnce<(V, U)>, U> {
-    v: V,
+use tuplex::*;
+use std::marker::Tuple;
+struct Curry<T: PopFront + Tuple, F: FnOnce<T>> where T::Remain: PushFront<T::Front> {
+    v: T::Front,
     f: F,
-    _phantom: PhantomData<U>
 }
 
-impl<V, U, F: FnOnce<(V, U)>> FnOnce<(U,)> for Curry<V, F, U> {
+impl<T: PopFront + Tuple, F: FnOnce<T>> FnOnce<T::Remain> for Curry<T, F> where T::Remain: Tuple + PushFront<T::Front, Output = T> {
     type Output = F::Output;
-    extern "rust-call" fn call_once(self, args: (U,)) -> Self::Output {
-        self.f.call_once((self.v, args.0))
+    extern "rust-call" fn call_once(self, args: T::Remain) -> Self::Output {
+        self.f.call_once(args.push_front(self.v))
     }
 }
 
-impl<V: Clone, U, F: FnMut<(V, U)>> FnMut<(U,)> for Curry<V, F, U> {
-    extern "rust-call" fn call_mut(&mut self, args: (U,)) -> Self::Output {
-        self.f.call_mut((self.v.clone(), args.0))
+impl<T: PopFront + Tuple , F: FnMut<T>> FnMut<T::Remain> for Curry<T, F> where T::Front: Clone, T::Remain: Tuple + PushFront<T::Front, Output = T> {
+    extern "rust-call" fn call_mut(&mut self, args: T::Remain) -> Self::Output {
+        self.f.call_mut(args.push_front(self.v.clone()))
     }
 }
 
-impl<V: Clone, U, F: Fn<(V, U)>> Fn<(U,)> for Curry<V, F, U> {
-    extern "rust-call" fn call(&self, args: (U,)) -> Self::Output {
-        self.f.call((self.v.clone(), args.0))
+impl<T: PopFront + Tuple, F: Fn<T>> Fn<T::Remain> for Curry<T, F> where T::Front: Clone, T::Remain: Tuple + PushFront<T::Front, Output = T> {
+    extern "rust-call" fn call(&self, args: T::Remain) -> Self::Output {
+        self.f.call(args.push_front(self.v.clone()))
     }
 }
 
-fn curry<V, F: FnOnce<(V, U)>, U>(v: V, f: F) -> Curry<V, F, U> {
-    Curry { v, f, _phantom: PhantomData }
+fn curry<T: PopFront + Tuple, F: FnOnce<T>>(v: T::Front, f: F) -> Curry<T, F> where T::Remain: PushFront<T::Front> {
+    Curry { v, f }
+}
+
+struct Nothing;
+
+impl FnOnce<()> for Nothing {
+    type Output = Nothing;
+    extern "rust-call" fn call_once(self, _args: ()) -> Self::Output {
+        Nothing
+    }
+}
+
+struct Something<T>(T);
+
+impl<T> FnOnce<()> for Something<T> {
+    type Output = Something<T>;
+    extern "rust-call" fn call_once(self, _args: ()) -> Self::Output {
+        self
+    }
 }
 
 fn main() {
-    let add = |x, y| x+y;
+    let add = |x, y, z| x+y+z;
     let curried = curry(5, add);
-    let res = curried(6);
+    let res = curried(6, 2);
     println!("{}", res);
+    let _n = Nothing()()()()()()()()()();
+    let _s = Something(6)()()()()()()();
 }
