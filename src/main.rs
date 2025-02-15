4ba0ececa8d23f8b086bf7d308843e8bd80a6acc
Aras14HD <aras14k5@gmail.com>
2025-02-15 20:56:35 +0100

    add further multicurrying

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,72 @@
 #![feature(fn_traits, unboxed_closures, tuple_trait)]
 
+use std::{
+    collections::btree_set::Union,
+    marker::Tuple,
+    ops::{AddAssign, Deref, DerefMut},
+};
 use tuplex::*;
-use std::{marker::Tuple, ops::{AddAssign, Deref, DerefMut}};
-struct Curry<T: PopFront + Tuple, F: FnOnce<T>> where T::Remain: PushFront<T::Front> {
+struct Curry<T: PopFront + Tuple, F: FnOnce<T>>
+where
+    T::Remain: PushFront<T::Front>,
+{
     v: T::Front,
     f: F,
 }
 
-impl<T: PopFront + Tuple, F: FnOnce<T>> FnOnce<T::Remain> for Curry<T, F> where T::Remain: Tuple + PushFront<T::Front, Output = T> {
+impl<T: PopFront + Tuple, F: FnOnce<T>> FnOnce<T::Remain> for Curry<T, F>
+where
+    T::Remain: Tuple + PushFront<T::Front, Output = T>,
+{
     type Output = F::Output;
     extern "rust-call" fn call_once(self, args: T::Remain) -> Self::Output {
         self.f.call_once(args.push_front(self.v))
     }
 }
 
-impl<T: PopFront + Tuple , F: FnMut<T>> FnMut<T::Remain> for Curry<T, F> where T::Front: Clone, T::Remain: Tuple + PushFront<T::Front, Output = T> {
+impl<T: PopFront + Tuple, F: FnMut<T>> FnMut<T::Remain> for Curry<T, F>
+where
+    T::Front: Clone,
+    T::Remain: Tuple + PushFront<T::Front, Output = T>,
+{
     extern "rust-call" fn call_mut(&mut self, args: T::Remain) -> Self::Output {
         self.f.call_mut(args.push_front(self.v.clone()))
     }
 }
 
-impl<T: PopFront + Tuple, F: Fn<T>> Fn<T::Remain> for Curry<T, F> where T::Front: Clone, T::Remain: Tuple + PushFront<T::Front, Output = T> {
+impl<T: PopFront + Tuple, F: Fn<T>> Fn<T::Remain> for Curry<T, F>
+where
+    T::Front: Clone,
+    T::Remain: Tuple + PushFront<T::Front, Output = T>,
+{
     extern "rust-call" fn call(&self, args: T::Remain) -> Self::Output {
         self.f.call(args.push_front(self.v.clone()))
     }
 }
 
-fn curry<T: PopFront + Tuple, F: FnOnce<T>>(v: T::Front, f: F) -> Curry<T, F> where T::Remain: PushFront<T::Front> {
+fn curry<T: PopFront + Tuple, F: FnOnce<T>>(v: T::Front, f: F) -> Curry<T, F>
+where
+    T::Remain: PushFront<T::Front>,
+{
     Curry { v, f }
+}
+
+macro_rules! multicurry {
+    (($head:expr, $tail:expr),$f:expr) => {
+        multicurry!(($head, $tail,), $f)
+    };
+    (($head:expr, $tail:expr,),$f:expr) => {
+        curry($head, multicurry!(($tail,), $f))
+    };
+    (($arg:expr), $f:expr) => {
+        multicurry!(($arg,), $f)
+    };
+    (($arg:expr,), $f:expr) => {
+        curry($arg, $f)
+    };
+    ($arg:expr, $f:expr) => {
+        multicurry!(($arg,), $f)
+    };
 }
 
 struct Nothing;
@@ -81,7 +120,7 @@ impl<T> Deref for Reverse<T> {
     fn deref(&self) -> &Self::Target {
         &self.0
     }
-} 
+}
 
 impl<T> DerefMut for Reverse<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
@@ -90,7 +129,7 @@ impl<T> DerefMut for Reverse<T> {
 }
 
 fn main() {
-    let add = |x, y, z| x+y+z;
+    let add = |x, y, z| x + y + z;
     let curried = curry(5, add);
     let res = curried(6, 2);
     println!("{}", res);
@@ -98,6 +137,9 @@ fn main() {
     let _s = Something(6)()()()()()()();
     let mut rev = Reverse(10);
     rev.add_assign(1);
-    let res = rev(|x| x+5);
+    let res = rev(|x| x + 5);
+    println!("{res}");
+    let curried = |z| add(5, 6, z);
+    let res = curried(2);
     println!("{res}");
 }
